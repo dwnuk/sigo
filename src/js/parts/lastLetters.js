f1029820732b1f961cc4765bf514e9346088e6c1
danielw12 <d.wnuk12@gmail.com>
2024-01-23 12:10:28 +0100

    up

--- src/js/parts/lastLetters.js
+++
@@ -1,8 +0,0 @@
-let paragraphs = document.querySelectorAll('p');
-paragraphs.forEach(function(p) {
-    var textReplace = p.innerHTML;
-   textReplace = textReplace.replace(/(\s)([\S])[\s]+/g, "$1$2&nbsp;");
-   textReplace = textReplace.replace(/(\s)([^<][\S]{1})[\s]+/g,"$1$2&nbsp;");
-   p.innerHTML = textReplace;
-});
-
--- src/js/parts/lazy.js
+++
@@ -1,5 +0,0 @@
-import LazyLoad from "vanilla-lazyload";
-const myLazyLoad = new LazyLoad({
- /* other options here */
- elements_selector: "img" // ADD THIS OPTION
-});
